f78bbe776f96e7d28a5d59acc9266d25eea30e8c
Ernie Phillips <48948846+ephillips2686@users.noreply.github.com>
2019-11-17 12:13:42 -0500

    added a db to store blob but command not adding varbinary to db

--- ConvertFilesToBLOB/ConvertForm.cs
+++ ConvertFilesToBLOB/ConvertForm.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.IO;
 using System.Linq;
@@ -26,13 +27,15 @@ namespace ConvertFilesToBLOB
         private void button1_Click(object sender, EventArgs e)
         {
             FolderBrowserDialog fbd = new FolderBrowserDialog();
-            if(fbd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            if (fbd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
             {
                 var path = fbd.SelectedPath;
 
                 string[] files = Directory.GetFiles(path);
                 List<byte[]> blobList = new List<byte[]>();
+
                 string message = "";
+
                 foreach (var file in files)
                 {
                     byte[] fileBytes = File.ReadAllBytes(file);
@@ -40,10 +43,43 @@ namespace ConvertFilesToBLOB
                     message += file + "\n";
                 }
 
-                foreach(byte[] blob in blobList)
+
+
+                //open db connection for insert of BLOB
+                using (SqlConnection cn = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\DBmdf.mdf;Integrated Security=True"))
                 {
-                    System.Diagnostics.Debug.WriteLine(blob);
+                    using (SqlCommand cmd = new SqlCommand())
+                    {
+                        foreach (byte[] blob in blobList)
+                        {
+                            cmd.CommandText = "INSERT INTO BlobStorage([File], FileName) VALUES(@param1, @param2)";
+                            cmd.Parameters.AddWithValue("@param1", blob);
+                            cmd.Parameters.AddWithValue("@param2", "filename");
+                            cmd.Connection = cn; //this was where the error originated in the first place.
+                            cn.Open();
+                            cmd.ExecuteNonQuery();
+                        }
+                    }
                 }
+
+                //string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\DBmdf.mdf;Integrated Security=True";
+                //using (SqlConnection con = new SqlConnection(connectionString))
+                //{
+                //    foreach (byte[] blob in blobList)
+                //    {
+                //        string sql = "INSERT INTO BlobStorage([File], FileName) VALUES(@param1, @param2)";
+                //        using (SqlCommand cmd = new SqlCommand(sql, con))
+                //        {
+                //            con.Open();
+
+                //            cmd.Parameters.Add("@param1", SqlDbType.VarBinary).Value = blob;
+                //            cmd.Parameters.Add("@param2", SqlDbType.NVarChar, 100).Value = "filename";
+                //            cmd.CommandType = CommandType.Text;
+                //            cmd.ExecuteNonQuery();
+                //        }
+                //    }
+                //    con.Close();
+                //}
 
                 MessageBox.Show(message);
             }
