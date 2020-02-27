4daf811e200988ca5c616812e4c94b5032aab6dd
Ernie Phillips <48948846+ephillips2686@users.noreply.github.com>
2020-02-26 19:12:50 -0500

    adding

--- ConvertFilesToBLOB/ConvertForm.cs
+++ ConvertFilesToBLOB/ConvertForm.cs
@@ -27,47 +27,78 @@ namespace ConvertFilesToBLOB
         private void button1_Click(object sender, EventArgs e)
         {
             List<FileObject> files = new List<FileObject>();
-
             using (SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\phill\Source\Repos\ConvertFilesToBLOB-WindowsFormsApp\ConvertFilesToBLOB\DBmdf.mdf;Integrated Security=True"))
             {
                 con.Open();
-                string query = "SELECT * FROM PathStorage";
-                using (SqlCommand cmd = new SqlCommand(query, con))
+                List<string> tables = new List<string>();
+                DataTable dt = con.GetSchema("Tables");
+                foreach (DataRow row in dt.Rows)
                 {
-                    cmd.CommandType = CommandType.Text;
-                    using (SqlDataReader dr = cmd.ExecuteReader())
-                    {
-                        while (dr.Read())
-                        {
-                            FileObject fo = new FileObject();
-                            fo.fileData = File.ReadAllBytes(dr["FilePath"].ToString());
-                            fo.fileName = dr["FileName"].ToString();
-                            files.Add(fo);
-                        }
-                            
-                    }
+                    string tablename = (string)row[2];
+                    tables.Add(tablename);
                 }
+                MessageBox.Show(tables.ToString(), "", "", MessageBoxButtons.);
+                //string query = "SELECT * FROM PathStorage";
+                //using (SqlCommand cmd = new SqlCommand(query, con))
+                //{
+                //    cmd.CommandType = CommandType.Text;
+                //    using (SqlDataReader dr = cmd.ExecuteReader())
+                //    {
+                //        while (dr.Read())
+                //        {
+                //            FileObject fo = new FileObject();
+                //            fo.fileData = File.ReadAllBytes(dr["FilePath"].ToString());
+                //            fo.fileName = dr["FileName"].ToString();
+                //            files.Add(fo);
+                //        }
+
+                //    }
+                //}
                 con.Close();
             }
 
-            //open db connection for insert of BLOB
-            using (SqlConnection cn = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\phill\Source\Repos\ConvertFilesToBLOB-WindowsFormsApp\ConvertFilesToBLOB\DBmdf.mdf;Integrated Security=True"))
-            {
-                cn.Open();
-                var command = "INSERT INTO BlobStorage(FileData, FileName) VALUES(@param1, @param2)";
-                using (SqlCommand cmd = new SqlCommand(command, cn))
-                {
-                    foreach (FileObject fo in files)
-                    {
-                        cmd.Parameters.Add("@param1", SqlDbType.VarBinary, -1).Value = fo.fileData;
-                        cmd.Parameters.Add("@param2", SqlDbType.NVarChar, 100).Value = fo.fileName;
-                        cmd.ExecuteNonQuery();
-                        cmd.Parameters.Clear();
-                    }
-                }
-            }
+            #region Direct Table Hits
+            //using (SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\phill\Source\Repos\ConvertFilesToBLOB-WindowsFormsApp\ConvertFilesToBLOB\DBmdf.mdf;Integrated Security=True"))
+            //{
+            //    con.Open();
+            //    string query = "SELECT * FROM PathStorage";
+            //    using (SqlCommand cmd = new SqlCommand(query, con))
+            //    {
+            //        cmd.CommandType = CommandType.Text;
+            //        using (SqlDataReader dr = cmd.ExecuteReader())
+            //        {
+            //            while (dr.Read())
+            //            {
+            //                FileObject fo = new FileObject();
+            //                fo.fileData = File.ReadAllBytes(dr["FilePath"].ToString());
+            //                fo.fileName = dr["FileName"].ToString();
+            //                files.Add(fo);
+            //            }
 
-            /*****************OPEN A FOLDER and CONVERT ALL FILES IN THAT FOLDER TO BLOB********************/
+            //        }
+            //    }
+            //    con.Close();
+            //}
+
+            ////open db connection for insert of BLOB
+            //using (SqlConnection cn = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\phill\Source\Repos\ConvertFilesToBLOB-WindowsFormsApp\ConvertFilesToBLOB\DBmdf.mdf;Integrated Security=True"))
+            //{
+            //    cn.Open();
+            //    var command = "INSERT INTO BlobStorage(FileData, FileName) VALUES(@param1, @param2)";
+            //    using (SqlCommand cmd = new SqlCommand(command, cn))
+            //    {
+            //        foreach (FileObject fo in files)
+            //        {
+            //            cmd.Parameters.Add("@param1", SqlDbType.VarBinary, -1).Value = fo.fileData;
+            //            cmd.Parameters.Add("@param2", SqlDbType.NVarChar, 100).Value = fo.fileName;
+            //            cmd.ExecuteNonQuery();
+            //            cmd.Parameters.Clear();
+            //        }
+            //    }
+            //}
+            #endregion
+
+            #region OPEN A FOLDER and CONVERT ALL FILES IN THAT FOLDER TO BLOB
             //FolderBrowserDialog fbd = new FolderBrowserDialog();
             //if (fbd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
             //{
@@ -112,6 +143,7 @@ namespace ConvertFilesToBLOB
 
             //    MessageBox.Show(message);
             //}
+            #endregion
         }
     }
 }
